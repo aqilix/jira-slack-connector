aa86f3820d8058732d130501c8f3eca87b0303a6
Dolly Aswin <dolly.aswin@gmail.com>
2016-10-11 11:35:00 +0700

    Use Environment Variable to connect with Redis

--- public/example.php
+++ public/example.php
@@ -1,8 +1,4 @@
 <?php
 require '../vendor/autoload.php';
-$client = new \Predis\Client([
-    'scheme' => 'tcp',
-    'host'   => 'redis',
-    'port'   => 6379,
-]);
+$client = new \Predis\Client(getenv('REDIS_URL'));
 echo $client->get('project_name');
